dc47e318f5632a334b0f8dbf7808565cc7266e20
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-09-21 15:24:40 -0500

    Create Category model

--- models/Category.js
+++ models/Category.js
@@ -7,8 +7,19 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
+    // define configuration
     sequelize,
     timestamps: false,
     freezeTableName: true,
