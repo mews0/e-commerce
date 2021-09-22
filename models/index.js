248bcd2701fdd452c250debd46e82baacd54cb6a
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-09-22 17:34:50 -0500

    Add ProductTag associations

--- models/index.js
+++ models/index.js
@@ -4,25 +4,47 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Product belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 
-// Categories have many Products
+// Category hasMany Products
 Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 
-// Products belongToMany Tags (through ProductTag)
+// Product belongsToMany Tag (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id'
 });
 
-// Tags belongToMany Products (through ProductTag)
+// Tag belongsToMany Product (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
+// ProductTag belongsTo Product
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+});
+
+// Product hasMany ProductTag
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id'
+});
+
+// ProductTag belongsTo Tag
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id',
+  onDelete: 'SET NULL'
+});
+
+// Tag hasMany ProductTag
+Tag.hasMany(ProductTag, {
   foreignKey: 'tag_id'
 });
 
